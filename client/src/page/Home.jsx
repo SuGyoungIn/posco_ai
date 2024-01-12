93de53f21bab282e6a607d675cda4cef8c107afe
tooli38 <tooli38@gmail.com>
2024-01-12 11:33:00 +0900

    입장하기 modal css수정

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -5,6 +5,7 @@ import ImageInput from '../components/ImageInput';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Topbar from "./global/Topbar";
 import {MyProSidebarProvider} from "./global/sidebar/sidebarContext";
+import Box from "@mui/material/Box";
 
 const divStyle = {
     height: '80vh',
@@ -20,12 +21,15 @@ function Home(props) {
         }
     }, []);
     return (
+        <Box m="20px">
             <div>
                 <Header title="공원 입장 페이지" subtitle="반려동물을 찍어 입장 해주시기 바랍니다."/>
+
                 <div style={divStyle}>
                     <ImageInput/>
                 </div>
             </div>
+        </Box>
     );
 }
 
